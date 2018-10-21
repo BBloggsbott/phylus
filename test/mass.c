6904a5c3b9afb47704d8f521e45c6d9982bad7d9
BBloggsbott <vishal27.rs@gmail.com>
2018-10-21 18:46:13 +0530

    Modify repo for RUST

--- test/mass.c
+++
@@ -1,9 +0,0 @@
-#include "./../src/object/mass.h"
-int
-main()
-{
-	mass_t myMass = getMass();
-	printMass(myMass);
-	removeMass(myMass);
-	return 0;
-}
--- test/object.c
+++
@@ -1,9 +0,0 @@
-#include "./../src/object/object.h"
-int
-main()
-{
-	object_t myObject = getObject();
-	printObject(myObject);
-	removeObject(myObject);
-	return 0;
-}
